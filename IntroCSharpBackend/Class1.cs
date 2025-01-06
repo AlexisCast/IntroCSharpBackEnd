bc7bbfa972a174987219bfd82bed291291cbfea6
Alexis Castellanos <castellanos.alexis@hotmail.com>
2025-01-06 01:37:14 -0600

    feat: 1 Intro - Creation of Objects

--- IntroCSharpBackend/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IntroCSharpBackend
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ IntroCSharpBackend/Program.cs
@@ -0,0 +1,19 @@
+﻿Sale sale = new Sale(15);
+var message = sale.GetInfo();
+
+Console.WriteLine(message);
+
+public class Sale
+{
+    public decimal Total { get; set; }
+    private decimal _some;
+    public Sale(decimal total)
+    {
+        this.Total = total;
+        _some = 8;
+    }
+    public string GetInfo()
+    {
+        return "The total is " + Total;
+    }
+}
