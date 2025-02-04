71d10443bde2da682ec19ead8b757f8a5a9644a1
Alexis Castellanos <castellanos.alexis@hotmail.com>
2025-02-04 01:46:47 -0600

    feat: 23 Dependency Injection - Testing Types of Dependency Injection

---
+++ CSharpBackend/Services/RandomService.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpBackend.Services
+{
+    public class RandomService : IRandomService
+    {
+        private readonly int _value;
+
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+
+        public RandomService()
+        {
+            _value = new Random().Next(1000);
+        }
+    }
+}
