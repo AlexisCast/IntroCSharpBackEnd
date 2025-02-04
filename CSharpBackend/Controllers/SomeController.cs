b154a1902345373f999c667e5941c4c8cf74fc90
Alexis Castellanos <castellanos.alexis@hotmail.com>
2025-02-04 02:55:41 -0600

    feat: 25 Asynchronous Programming - Create Synchronous Method

---
+++ CSharpBackend/Controllers/SomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet("sync")]
+        public IActionResult GetSync()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            Thread.Sleep(2500);
+            Console.WriteLine("Conection to a DB finished...");
+
+            Thread.Sleep(2500);
+            Console.WriteLine("Email sent finished...");
+
+            Console.WriteLine("Everything has finished...");
+
+            stopwatch.Stop();
+
+            return Ok(stopwatch.Elapsed);
+        }
+    }
+}
