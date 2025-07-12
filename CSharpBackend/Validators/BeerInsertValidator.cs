4aa126e55afd7d6fc8ff535ae163bed4877fafda
Alexis Castellanos <castellanos.alexis@hotmail.com>
2025-07-12 00:25:26 -0600

    feat: 48 Validations - Personalized Message Error

--- CSharpBackend/Validators/BeerInsertValidator.cs
+++ CSharpBackend/Validators/BeerInsertValidator.cs
@@ -8,7 +8,15 @@ namespace CSharpBackend.Validators
         public BeerInsertValidator()
         {
             RuleFor(x => x.Name)
-                .NotEmpty();
+                .NotEmpty().WithMessage("Name field is required");
+            RuleFor(x => x.Name)
+                .Length(3, 20).WithMessage("Name must be between 3 and 20 characters long");
+            RuleFor(x => x.BrandId)
+                .NotNull().WithMessage("BrandId must is required");
+            RuleFor(x => x.BrandId)
+                .GreaterThan(0).WithMessage("Error with the BrandId");
+            RuleFor(x => x.Alcohol)
+                .GreaterThan(0).WithMessage("The {PropertyName} must be greater than 0");
         }
     }
 }
